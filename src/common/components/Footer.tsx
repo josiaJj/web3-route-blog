688b7ecb9832431a60d52284805f1c4dbc205773
josiaJj <hei.josia@gmail.com>
2024-03-11 12:47:03 +0300

    feat: route

--- src/common/components/Footer.tsx
+++ src/common/components/Footer.tsx
@@ -1,7 +1,7 @@
 export function Footer(){
   return (
     <footer className="w-full h-[50px] text-white bg-red-800 flex items-center justify-center">
-      MY CUTE FOOTER COLORED RED
+      copyright
     </footer>
   )
 }
--- src/common/components/Header.tsx
+++ src/common/components/Header.tsx
@@ -1,7 +1,7 @@
 export function Header(){
   return (
     <nav className="w-full h-[50px] text-white bg-green-800 flex items-center justify-center">
-      MY CUTE FOOTER COLORED GREEN 
+      HEADER
     </nav>
   )
 }
